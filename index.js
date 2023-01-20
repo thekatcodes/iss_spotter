82d0996c861f7f983857ca6eeeb77f41c52e62a4
Katie Liu <katieliu59@gmail.com>
2023-01-19 22:41:04 -0500

    Implement function using npm request to fetch our IP address via ipify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
 fetchMyIP((error, ip) => {
 	if (error) {
@@ -7,4 +7,12 @@ fetchMyIP((error, ip) => {
 	}
 
 	console.log("It worked! Returned IP:", ip);
+});
+
+fetchCoordsByIP("96.21.209.140", (error, geoLocation) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked!", geoLocation);
 });
--- iss.js
+++ iss.js
@@ -8,30 +8,56 @@ const request = require("request");
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
- const fetchMyIP = function(callback) { 
-    request('https://api.ipify.org?format=json', function (error, response, body) {
-      console.error('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        if (error) {
-            callback(error, null);
-            return;
-          }
-          // if non-200 status, assume server error
-          if (response.statusCode !== 200) {
-            const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-            callback(Error(msg), null);
-            return;
-          }
-        const ip = JSON.parse(body);
-        // console.log(ip)
+const fetchMyIP = function (callback) {
+	request(
+		"https://api.ipify.org?format=json",
+		function (error, response, body) {
+			// console.error("error:", error); // Print the error if one occurred
+			// console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+			// console.log("body:", body); // Print the HTML for the Google homepage.
+			if (error) {
+				callback(error, null);
+				return;
+			}
+			// if non-200 status, assume server error
+			if (response.statusCode !== 200) {
+				const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+				callback(Error(msg), null);
+				return;
+			}
+			const ip = JSON.parse(body);
+			// console.log(ip)
 
-        if (error) {
-            callback(error);
-        } else {
-            callback(null, ip)
-        }
-    });  
-}
-  
-  module.exports = { fetchMyIP };
+			if (error) {
+				callback(error);
+			} else {
+				callback(null, ip);
+			}
+		}
+        );
+    };
+    
+    const fetchCoordsByIP = function(ipString, callback) {
+        request(
+           `http://ipwho.is/${ipString}`,
+            function(error, response, body) {
+                // console.error("error:", error); // Print the error if one occurred
+                // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+                // console.log("body:", body); // Print the HTML for the Google homepage.
+                
+                const ipInfo = JSON.parse(body);
+                const latitude = ipInfo.latitude;
+                const longitude = ipInfo.longitude;
+                const geoObj = {latitude,longitude}
+                // console.log(lat)
+
+                if (error) {
+                    callback(error);
+                } else {
+                    callback(null, geoObj);
+                }
+            }
+        );
+    };
+    
+module.exports = { fetchMyIP, fetchCoordsByIP };
