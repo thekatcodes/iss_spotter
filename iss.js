00db875d7262453b0aa3f7b8581cfe59f606e11d
Katie Liu <katieliu59@gmail.com>
2023-01-19 22:48:19 -0500

    Implement function to retrieve lagitude and longitude via ipwho.is via the previously retrieved IP address, and implement error handling logic

--- iss.js
+++ iss.js
@@ -34,30 +34,32 @@ const fetchMyIP = function (callback) {
 				callback(null, ip);
 			}
 		}
-        );
-    };
-    
-    const fetchCoordsByIP = function(ipString, callback) {
-        request(
-           `http://ipwho.is/${ipString}`,
-            function(error, response, body) {
-                // console.error("error:", error); // Print the error if one occurred
-                // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-                // console.log("body:", body); // Print the HTML for the Google homepage.
-                
-                const ipInfo = JSON.parse(body);
-                const latitude = ipInfo.latitude;
-                const longitude = ipInfo.longitude;
-                const geoObj = {latitude,longitude}
-                // console.log(lat)
+	);
+};
 
-                if (error) {
-                    callback(error);
-                } else {
-                    callback(null, geoObj);
-                }
-            }
-        );
-    };
-    
+const fetchCoordsByIP = function (ipString, callback) {
+	request(`http://ipwho.is/${ipString}`, function (error, response, body) {
+		// console.error("error:", error); // Print the error if one occurred
+		// console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+		// console.log("body:", body); // Print the HTML for the Google homepage.
+
+		const ipInfo = JSON.parse(body);
+		const latitude = ipInfo.latitude;
+		const longitude = ipInfo.longitude;
+		const geoObj = { latitude, longitude };
+		// console.log(lat)
+		if (response.statusCode !== 200) {
+			const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+			callback(Error(msg), null);
+			return;
+		}
+
+		if (error) {
+			callback(error);
+		} else {
+			callback(null, geoObj);
+		}
+	});
+};
+
 module.exports = { fetchMyIP, fetchCoordsByIP };
